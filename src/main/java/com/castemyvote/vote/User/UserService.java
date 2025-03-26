d3ab16d0ac39f700da59ab37fdb6193b59081715
achaltyagi18 <dhirashtyagi@gmail.com>
2025-03-26 15:09:02 +0530

    Create UserService.java

---
+++ src/main/java/com/castemyvote/vote/User/UserService.java
@@ -0,0 +1,10 @@
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
