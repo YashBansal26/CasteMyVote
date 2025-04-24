1eb4f705d8906fe4b5d8d2e76e732f654218bead
ShwaasAWahal <72555602+ShwaasAWahal@users.noreply.github.com>
2025-04-24 08:33:34 +0530

    Delete src directory

--- src/main/java/com/castemyvote/vote/Candidate/CandidateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.castemyvote.vote.Candidate;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandidateRepository extends JpaRepository<Candidate , Long> {
-}
